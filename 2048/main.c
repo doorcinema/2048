def9f4d8936ce926fc6235553ee9817334d36291
doorcinema <doorcinema0000@gmail.com>
2022-07-27 00:15:08 +0900

    7.26

--- 2048/main.c
+++ 2048/main.c
@@ -47,12 +47,18 @@ size_t arr_size(int *arr) {
 void print_arr(int *arr, size_t size) {
 	for (size_t i = 0; i < size * size; i++) {
 		if (*(arr + i) == 0)
-			printf("    ");
-		else
-			printf("%d", *(arr + i));
+			printf("|    |");
+		else 
+			printf("|%d|", *(arr + i));
 		if ((i + 1) % size == 0)
 			printf("\n");
 	}
+	//for (size_t row = 0; row < size; row++) {
+	//	for (size_t col = 0; col < size; col++) {
+	//		printf("0");
+	//	}
+	//	printf("\n");
+	//}
 
 	return;
 }
@@ -62,24 +68,88 @@ int my_rand(size_t size) {
 	return value;
 }
 
-void input_keyboard() {
-	char c;
+void move_up(int *arr, size_t size) {
+	for (size_t row = 0; row < size; row++) {
+		for (size_t col = 0; col < size; col++) {
+			if (row == 0)
+				continue;
+			else if (*(arr + col + (row - 1) * size) != 0)
+				continue;
+			else {
+				*(arr + col + (row - 1) * size) = *(arr + col + row * size);
+				*(arr + col + row * size) = 0;
+			}
+		}
+	}
+}
 
-	while (1) {
+void move_down(int *arr, size_t size) {
+	for (size_t row = size; row > 0; row--) {
+		for (size_t col = size; col > 0; col--) {
+			if (row == size)
+				continue;
+			else if (*(arr + col + (row - 1) * size) != 0)
+				continue;
+			else {
+				*(arr + col + (row - 1) * size) = *(arr + col + row * size);
+				*(arr + col + (row - 1) * size) = 0;
+			}
+		}
+	}
+}
+
+void move_left(int *arr, size_t size) {
+	for (size_t row = 0; row < size; row++) {
+		for (size_t col = 0; col < size; col++) {
+			if (col == 0)
+				continue;
+			else if (*(arr + (col - 1) + row * size) != 0)
+				continue;
+			else {
+				*(arr + (col - 1) + row * size) = *(arr + col + row * size);
+				*(arr + col + row * size) = 0;
+			}
+		}
+	}
+}
+
+void move_right(int *arr, size_t size) {
+	for (size_t row = 0; row < size; row++) {
+		for (size_t col = 0; col < size; col++) {
+			if (col == size - 1)
+				continue;
+			else if (*(arr + (col + 1) + row * size) != 0)
+				continue;
+			else {
+				*(arr + (col + 1) + row * size) = *(arr + col + row * size);
+				*(arr + col + row * size) = 0;
+			}
+		}
+	}
+}
+
+void input_keyboard(int* arr, size_t size) {
+	char c = 0;
+
+	while (c == 0) {
 		while (_kbhit()) {
 			c = _getch();
 			switch (c) {
 			case 72:
-				printf("UP\n");
+				//printf("UP");
+				move_up(arr, size);
 				break;
 			case 75:
-				printf("LEFT\n");
+				//printf("LEFT\n");
+				move_left(arr, size);
 				break;
 			case 77:
-				printf("RIGHT\n");
+				//printf("RIGHT\n");
+				move_right(arr, size);
 				break;
 			case 80:
-				printf("DOWN\n");
+				//printf("DOWN\n");
+				move_down(arr, size);
 				break;
 			}
 		}
@@ -93,14 +163,34 @@ int main(void) {
 
 	int *arr = make_arr();
 	size_t size = arr_size(arr);
-	size_t total_size = (size_t)pow((double)size, (double)2);
+	//size_t total_size = (size_t)pow((double)size, (double)2);
 
 	for (size_t i = 0; i < size * size; i++)
 		*(arr + i) = 0;
 
 	print_arr(arr, size);
 
-	input_keyboard();
+	printf("\n\n");
+
+	for (size_t i = 0; i < size * size; i++)
+		if (i % 5 == 0)
+			*(arr + i) = 1024;
+
+	*(arr + 4) = 1024;
+
+	print_arr(arr, size);
+
+	printf("\n\n");
+
+	input_keyboard(arr, size);
+
+	print_arr(arr, size);
+
+	printf("\n\n");
+
+	input_keyboard(arr, size);
+
+	print_arr(arr, size);
 
 	free(arr);
 	return 0;
